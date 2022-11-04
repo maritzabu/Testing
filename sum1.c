4252d11b34dd6ea403b869965f431654bd08d9a1
maritzabu <108763331+maritzabu@users.noreply.github.com>
2022-11-04 16:13:44 -0500

    Add files via upload

---
+++ sum1.c
@@ -0,0 +1,24 @@
+// gcc -Wall sum1.c -o sum1
+// ./sum1 4 8 -5 0 20
+// prints: Sum=27 Sum2=505 Sum3=8451
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static int sum1(int argc, char *argv[]) {
+  TBD
+}
+
+static int sum2(int argc, char *argv[]) {
+  TBD
+}
+
+static int sum3(int argc, char *argv[]) {
+  TBD
+}
+
+int main(int argc, char *argv[]) {
+  TBD
+  printf("Sum=%d Sum2=%d Sum3=%d\n", x, y, z);
+  return 0;
+}
